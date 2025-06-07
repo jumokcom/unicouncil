d57eb1bbe86f439bcd5b58668ea00f87b90a4a99
jumokcom <jumokcom2001@gmail.com>
2025-06-07 16:11:01 +0900

    프로젝트 초기 설정

--- app/albums/[id]/page.tsx
+++ app/albums/[id]/page.tsx
@@ -6,12 +6,19 @@
  * 동적 라우팅: [id] 파라미터로 앨범 식별
  */
 
-export default function AlbumDetailPage({ params }: { params: { id: string } }) {
+export default async function AlbumDetailPage({ 
+  params 
+}: { 
+  params: Promise<{ id: string }> 
+}) {
+  // Next.js 15에서 params는 Promise로 변경됨
+  const { id } = await params;
+  
   // 개별 앨범 상세 구현 예정
   return (
     <div>
       <h1>Album Detail Page</h1>
-      <p>Album ID: {params.id}</p>
+      <p>Album ID: {id}</p>
     </div>
   )
 }
